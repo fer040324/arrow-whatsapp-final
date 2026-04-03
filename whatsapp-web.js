64979f61c8bc06bd220dd1284ca98bf04c2b18cb
fer040324 <fernando040324@gmail.com>
2026-04-03 03:15:14 -0500

    Update whatsapp-web.js

--- whatsapp-web.js
+++ whatsapp-web.js
@@ -1,29 +1,20 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode');
+const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    }
+authStrategy: new LocalAuth(),
+puppeteer: {
+args: ['--no-sandbox','--disable-setuid-sandbox']
+}
 });
 
-client.on('qr', async (qr) => {
-    console.log('QR RECEIVED');
-
-    const url = await qrcode.toDataURL(qr);
-    console.log('SCAN THIS QR:');
-    console.log(url);
+client.on('qr', (qr) => {
+console.log('QR RECEIVED');
+qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-    console.log('BOT LISTO');
-});
-
-client.on('message', async msg => {
-    if (msg.body.toLowerCase() === 'hola') {
-        msg.reply('Hola 👋 soy la IA de Arrow Store');
-    }
+console.log('READY');
 });
 
 client.initialize();
