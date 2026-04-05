e88b8591f994ddb9ff0ec7ab209ab299ef6fd8dd
fer040324 <fernando040324@gmail.com>
2026-04-05 00:13:13 -0500

    Update whatsapp-web.js

--- whatsapp-web.js
+++ whatsapp-web.js
@@ -2,19 +2,35 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-authStrategy: new LocalAuth(),
-puppeteer: {
-args: ['--no-sandbox','--disable-setuid-sandbox']
-}
+    authStrategy: new LocalAuth({
+        dataPath: './session'
+    }),
+    puppeteer: {
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-accelerated-2d-canvas",
+            "--no-first-run",
+            "--no-zygote",
+            "--single-process",
+            "--disable-gpu"
+        ]
+    }
 });
 
 client.on('qr', (qr) => {
-console.log('QR RECEIVED');
-qrcode.generate(qr, { small: true });
+    console.log('================ QR ================');
+    qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-console.log('READY');
+    console.log('BOT CONECTADO');
+});
+
+client.on('authenticated', () => {
+    console.log('AUTENTICADO');
 });
 
 client.initialize();
